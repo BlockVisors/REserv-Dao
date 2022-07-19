df0fa5899ab48377b83421f6006240c4fc3ada55
dameon green <dameongreen@gmail.com>
2022-07-18 23:31:53 -0400

    first commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const RESERV_VIP_PASS_NFT_CONTRACT_ADDRESS =
+  "0x3dab1D1DFA16c3c85A77F7D5e6CBbA0a341E1bD0";
+
+module.exports = { RESERV_VIP_PASS_NFT_CONTRACT_ADDRESS };
